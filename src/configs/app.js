25d182ebbe66d722410b4054e77267801fd7502d
Olajuwon <olajuwonlawal2012@gmail.com>
2024-04-04 02:06:46 +0100

    Fix: rest endppoint

--- src/configs/app.js
+++ src/configs/app.js
@@ -1,8 +1,8 @@
 let REST_ENDPOINT
 
-REST_ENDPOINT = 'http://localhost:5454/api/v1'
+// REST_ENDPOINT = 'http://localhost:5454/api/v1'
 
-// REST_ENDPOINT = 'https://vend-payout.onrender.com/api/v1'
+REST_ENDPOINT = 'https://vend-payout.onrender.com/api/v1'
 
 const Config = {
   isDev: process.env.NODE_ENV === 'development',
